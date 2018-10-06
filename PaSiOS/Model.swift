8f2269ddc9c3ac49fb0e0769a849bca5cbbea052
Sweeper777 <sumulang@gmail.com>
2018-10-06 09:24:08 +0100

    added PortsAndSurveyorsData

--- PaSiOS/Model.swift
+++ PaSiOS/Model.swift
@@ -1,5 +1,23 @@
 import Foundation
 
+class PortsAndSurveyorsData: Codable {
+    var surveyors: [Surveyor]!
+    var ports: [Port]!
+    private var dictionary: [Int: Surveyor]!
+    
+    var surveyorsDictionary: [Int: Surveyor] {
+        if dictionary == nil {
+            dictionary = [Int: Surveyor](uniqueKeysWithValues: surveyors.map { ($0.id, $0) })
+        }
+        return dictionary
+    }
+    
+    enum CodingKeys : String, CodingKey {
+        case surveyors
+        case ports
+    }
+}
+
 struct Port: Codable {
     var name: String
     var latitude, longitude: Double
