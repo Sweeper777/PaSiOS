f4afcca9d8d8f29d571f55f1edaa011789d981e3
Sweeper777 <sumulang@gmail.com>
2018-10-07 09:19:02 +0100

    added pods

---
+++ Pods/Target Support Files/Pods-PaSiOS/Pods-PaSiOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PaSiOS : NSObject
+@end
+@implementation PodsDummy_Pods_PaSiOS
+@end
---
+++ Pods/Target Support Files/TCTableViewSearchController/TCTableViewSearchController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TCTableViewSearchController : NSObject
+@end
+@implementation PodsDummy_TCTableViewSearchController
+@end
