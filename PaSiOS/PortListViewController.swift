9bca445d55d9f16d5d50b14ea6907a7af4f4eb15
Sweeper777 <sumulang@gmail.com>
2018-10-09 07:08:26 +0100

    added data and ports properties

--- PaSiOS/PortListViewController.swift
+++ PaSiOS/PortListViewController.swift
@@ -4,4 +4,14 @@ import SCLAlertView
 
 class PortsListViewController: UITableViewController {
 
+    
+    var data: PortsAndSurveyorsData! {
+        didSet {
+            data.ports.sort(by: { $0.name < $1.name })
+        }
+    }
+    var ports: [Port] {
+        return data?.ports ?? []
+    }
+    
 }
