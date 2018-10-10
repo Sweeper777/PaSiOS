e13abff869d3f37f2bdd15133a8835beb267157c
Sweeper777 <sumulang@gmail.com>
2018-10-10 06:54:24 +0100

    retrieved data on viewDidLoad

--- PaSiOS/PortListViewController.swift
+++ PaSiOS/PortListViewController.swift
@@ -14,4 +14,22 @@ class PortsListViewController: UITableViewController {
         return data?.ports ?? []
     }
     
+    var filteredPorts: [Port] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let data = loadCache() {
+            self.data = data
+        } else {
+            downloadData { [unowned self] (data) in
+                if let pasData = data {
+                    self.data = pasData
+                    self.tableView.reloadData()
+                } else {
+                    let alert = SCLAlertView()
+                    alert.showError("Error", subTitle: "Unable to load data.")
+                }
+            }
+        }
+    }
 }
