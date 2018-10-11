ceab0b39f589103ca9883bfc32a0f76aec647d81
Sweeper777 <sumulang@gmail.com>
2018-10-11 06:53:58 +0100

    added mapView, data, ports, and markers properties

--- PaSiOS/MapViewController.swift
+++ PaSiOS/MapViewController.swift
@@ -3,4 +3,15 @@ import GoogleMaps
 import SCLAlertView
 
 class MapViewController: UIViewController {
+    var mapView: GMSMapView!
+    var data: PortsAndSurveyorsData! {
+        didSet {
+            data.ports.sort(by: { $0.name < $1.name })
+        }
+    }
+    
+    var ports: [Port] {
+        return data?.ports ?? []
+    }
+    var markers: [GMSMarker] = []
 }
