9269c9848101ea777883da99c0d052c0687f3eb8
Sweeper777 <sumulang@gmail.com>
2018-10-11 06:57:42 +0100

    added searchController and filteredPorts properties

--- PaSiOS/MapViewController.swift
+++ PaSiOS/MapViewController.swift
@@ -4,6 +4,8 @@ import SCLAlertView
 
 class MapViewController: UIViewController {
     var mapView: GMSMapView!
+    let searchController = UISearchController(searchResultsController: nil)
+
     var data: PortsAndSurveyorsData! {
         didSet {
             data.ports.sort(by: { $0.name < $1.name })
@@ -13,7 +15,11 @@ class MapViewController: UIViewController {
     var ports: [Port] {
         return data?.ports ?? []
     }
+    
+    var filteredPorts: [Port] = []
+    
     var markers: [GMSMarker] = []
+    
     override func viewDidLoad() {
         mapView = GMSMapView()
         mapView.setMinZoom(kGMSMinZoomLevel, maxZoom: 9)
