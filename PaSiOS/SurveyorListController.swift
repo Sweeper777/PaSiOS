555190ef0c0bb98aa3cbd3896f1382cab2791124
Sweeper777 <sumulang@gmail.com>
2018-10-14 09:14:30 +0100

    added some table view datasource methods

--- PaSiOS/SurveyorListController.swift
+++ PaSiOS/SurveyorListController.swift
@@ -3,4 +3,16 @@ import UIKit
 class SurveyorListController: UITableViewController {
     var portName: String!
     var surveyors: [Surveyor] = []
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "surveyors for port '\(portName!)'"
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return surveyors.count
+    }
 }
