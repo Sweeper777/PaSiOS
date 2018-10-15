0b62c28bf5ceaf19d4ad995f2cd0a784dc7172ff
Sweeper777 <sumulang@gmail.com>
2018-10-15 16:27:16 +0100

    added details controller

---
+++ PaSiOS/DetailsController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class DetailsController: UITableViewController {
+    var surveyor: Surveyor!
+    
+    @IBOutlet var priceTextView: UITextView!
+    @IBOutlet var contactTextView: UITextView!
+}
