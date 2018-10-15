8bf9a6a39bedc6a1352897f36e1a6807422430d9
Sweeper777 <sumulang@gmail.com>
2018-10-15 16:27:39 +0100

    set the details text views

--- PaSiOS/DetailsController.swift
+++ PaSiOS/DetailsController.swift
@@ -5,4 +5,9 @@ class DetailsController: UITableViewController {
     
     @IBOutlet var priceTextView: UITextView!
     @IBOutlet var contactTextView: UITextView!
+    
+    override func viewDidLoad() {
+        priceTextView.text = surveyor.prices.joined(separator: "\n")
+        contactTextView.text = surveyor.contacts.joined(separator: "\n")
+    }
 }
