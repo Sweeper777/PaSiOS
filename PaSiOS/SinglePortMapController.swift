40ca9e6718a76ca11a3645e0a76e36df1f7fde5f
Sweeper777 <sumulang@gmail.com>
2018-10-16 06:43:35 +0100

    initialised map and marker in single port map VC

--- PaSiOS/SinglePortMapController.swift
+++ PaSiOS/SinglePortMapController.swift
@@ -4,4 +4,14 @@ import GoogleMaps
 class SinglePortMapController : UIViewController {
     var mapView: GMSMapView!
     var port: Port!
+    
+    override func viewDidLoad() {
+        mapView = GMSMapView()
+        self.view = mapView
+        mapView.moveCamera(GMSCameraUpdate.setTarget(port.coordinate))
+        mapView.moveCamera(GMSCameraUpdate.zoom(to: 7))
+        
+        let marker = GMSMarker(position: port.coordinate)
+        marker.map = mapView
+    }
 }
